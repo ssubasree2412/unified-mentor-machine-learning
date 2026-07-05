e0cfc2c16b00c90b0d9c03789a3151797acfc91d
ssubasree2412 <ssubasreesankar2412@gmail.com>
2026-07-05 20:08:08 +0530

    utils.py

---
+++ utils.py
@@ -0,0 +1,22 @@
+"""
+Utility Functions
+"""
+import matplotlib.pyplot as plt
+
+
+def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
+    """ plots logistic data with two axis """
+    # Find Indices of Positive and Negative Examples
+    pos = y == 1
+    neg = y == 0
+    pos = pos.reshape(-1,)  #work with 1D or 1D y vectors
+    neg = neg.reshape(-1,)
+
+    # Plot examples
+    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c = 'green', label=pos_label)
+    ax.scatter(X[neg, 0], X[neg, 1], marker='.', s=s, label=neg_label, facecolors='none', edgecolors='#0096ff', lw=3)
+    ax.legend(loc=loc)
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
